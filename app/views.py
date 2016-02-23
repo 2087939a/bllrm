d96749222970fd39595243368a2b705dd076d11f
Terence Aqachmar <2087939a@student.gla.ac.uk>
2016-02-23 10:30:55 +0000

    23 fev

--- app/views.py
+++ app/views.py
@@ -1,7 +1,87 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-# Create your views here.
 
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import sys
+import os
+import urllib
+from keys import YOUTUBE_API_KEY, BOILER_ROOM_CHANNELID
+
+
+
+# Set API_KEY to the "API key" value from the "Access" tab of the
+# Google APIs Console http://code.google.com/apis/console#access
+# Please ensure that you have enabled the YouTube Data API and Freebase API
+# for your project.
+API_KEY = YOUTUBE_API_KEY 
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+QUERY_TERM = ""
+
+youtube = build(
+YOUTUBE_API_SERVICE_NAME, 
+YOUTUBE_API_VERSION, 
+developerKey=API_KEY
+)
+
+
+def search_by_keyword():
+
+    search_response = youtube.search().list(
+    part="snippet",
+    channelId = BOILER_ROOM_CHANNELID,
+    order = "viewCount",
+    maxResults=11
+    ).execute()
+    videos = []
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s" % search_result["id"]["videoId"])
+    
+    return videos
+
+videos = search_by_keyword()
+
+print videos
+
+print " -------------------------- "
+
+def search_video_stats(v):
+
+    search_response = youtube.search().list(
+    part="statistics, snippet",
+    videoId = v,
+    
+    
+    ).execute()
+    return search_response.get("items", [])
+
+def parse_videos(videos):
+    vs = []
+    for vid in videos:
+        list.append(search_video_stats(vid))
+    return list
+	# Create your views here.
+
+
+# parse_videos(videos)
+search_response = youtube.videos().list(
+part="statistics,snippet",
+id="vy-k0FopsmY",
+fields = "items(statistics(viewCount), snippet(title,publishedAt), id)"
+).execute()
+
+print search_response.get("items" , [])[0]["id"]
+
+print search_response.get("items" , [])
+
+# https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.videos.list?
+# part=statistics%252C+snippet&
+# id=vy-k0FopsmY&
+# fields=items(statistics%252C+snippet(publishedAt%252Ctitle%252Cthumbnails(default(url))))&
+# _h=1&
 
 def index(request):
 
@@ -14,3 +94,6 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
 
     return render(request, 'index.html')
+
+	
+	
--- populate.py
+++ populate.py
@@ -14,17 +14,89 @@ import gdata.youtube.service
 #use google api v3
 
 
-def GetAndPrintVideoFeed(uri):
-	yt_service = gdata.youtube.service.YouTubeService()
-	yt_service.ssl = True
-	feed = yt_service.GetYouTubeVideoFeed(uri)
-	for entry in feed.entry:
-		PrintEntryDetails(entry) # full documentation for this function
 
+
+
+from apiclient.discovery import build
+from apiclient.errors import HttpError
+from oauth2client.tools import argparser
+import sys
+import os
+import urllib
+from app.keys import YOUTUBE_API_KEY, BOILER_ROOM_CHANNELID
+
+
+
+# Set API_KEY to the "API key" value from the "Access" tab of the
+# Google APIs Console http://code.google.com/apis/console#access
+# Please ensure that you have enabled the YouTube Data API and Freebase API
+# for your project.
+API_KEY = YOUTUBE_API_KEY 
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+QUERY_TERM = ""
+
+youtube = build(
+YOUTUBE_API_SERVICE_NAME, 
+YOUTUBE_API_VERSION, 
+developerKey=API_KEY
+)
+
+
+def search_by_keyword():
+
+    search_response = youtube.search().list(
+    part="snippet",
+    channelId = BOILER_ROOM_CHANNELID,
+    order = "viewCount",
+    maxResults=11
+    ).execute()
+    videos = []
+    v = dict()
+    titles = []
+    for search_result in search_response.get("items", []):
+        if search_result["id"]["kind"] == "youtube#video":
+            videos.append("%s" % str(search_result["id"]["videoId"]))
+            v[search_result["id"]["videoId"]] = search_result["snippet"]["title"]
+            titles.append("%s" % search_result["snippet"]["title"])
+    print v
+    print titles
+    return videos
+
+videos = search_by_keyword()
+
+print videos
+
+print " -------------------------- "
+
+
+
+def parse_videos(videos):
+    d = dict()
+    for vid in videos:
+        d[vid] = get_stats(vid)
+    return d
+	# Create your views here.
+
+def get_stats(videoId):
+    # parse_videos(videos)
+    search_response = youtube.videos().list(
+    part="statistics,snippet",
+    id=videoId,
+    fields = "items(statistics(viewCount), snippet(title,publishedAt))"
+    ).execute()
+    return search_response.get("items" , [])[0]
+
+
+print parse_videos(videos)
 	
-uri = "https://gdata.youtube.com/feeds/api/videos?author=brtvofficial&orderby=viewCount&max-results=10&v=2"
-	
-GetAndPrintVideoFeed(uri)
+
+# https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.videos.list?
+# part=statistics%252C+snippet&
+# id=vy-k0FopsmY&
+# fields=items(statistics%252C+snippet(publishedAt%252Ctitle%252Cthumbnails(default(url))))&
+# _h=1&
+
 
 def add_video(name, url, videoid):
 	v = Video.objects.get_or_create(name = name)[0]
@@ -34,6 +106,6 @@ def add_video(name, url, videoid):
 	return v
 
 # Start execution here!
-#if __name__ == '__main__':
-#    print "Starting VideoPop population script..."
-#    populate()
+if __name__ == '__main__':
+    print "Starting population script..."
+    populate()
